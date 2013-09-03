9ab0518959b6549261dfd91f8e2a1db3b909972b
Campbell <campbellzc@s.dcsdk12.org>
2013-09-03 08:07:01 -0600

    Equivalent For Loop

--- Loops.java
+++ Loops.java
@@ -1,12 +1,14 @@
 public class Loops {
 	public static void main(String[] args) {
-		int x = 1; // start
-		while (x <= 100) { // stopping condition
-			System.out.println(x); // loop action
-			x++; // change
+		// int x = 1; // start
+		// while (x <= 100) { // stopping condition
+		// 	System.out.println(x); // loop action
+		// 	x++; // change
+		// }
+
+		for (int i=1; i <= 100; i++ ) {
+			System.out.println(i);
 		}
-
-
 
 	}
 	
